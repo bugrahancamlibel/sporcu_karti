a0b28ffec11282849f00b1aa9079083d764f9917
cagataykocyigit <cagatay.kocyigit@hotmail.com>
2020-11-20 20:10:24 +0300

    çok yoruldum push

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,6 +14,7 @@ public class Main {
         Basketbolcu basketbolcu = new Basketbolcu("catoski","bornova sports",45,77,99);
         System.out.println("adi: "+futbolcu.getFutbolcuAdi()+" penalti "+ futbolcu.getPenalti());
         System.out.println("adi: "+basketbolcu.getBasketbolcuAdi()+" ucluk degeri: "+basketbolcu.getUcluk());
+        System.out.println("abi ben cok yoruldum ");
 
     }
 }
