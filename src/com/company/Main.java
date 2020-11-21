65052802bc65e94132066dbada6b2cbafcacdb0e
cagataykocyigit <cagatay.kocyigit@hotmail.com>
2020-11-21 21:59:16 +0300

    bi commit atayım dedim abi bozuldu mu bozulmadı mı diye

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
         Futbolcu futbolcu1 = new Futbolcu();
+        System.out.println("sorunusuz bir commit olmasi dileklerimle");
     }
 }
