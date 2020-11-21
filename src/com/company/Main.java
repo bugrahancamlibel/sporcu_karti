b33e3b820d0a221520ed01b59035cd27551dcc10
cagataykocyigit <cagatay.kocyigit@hotmail.com>
2020-11-21 15:10:46 +0300

    Add Oyuncu,Bilgisayar,Kullanici classes

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Futbolcu futbolcu = new Futbolcu("meksi","barca",12,23,34);
-        Basketbolcu basketbolcu = new Basketbolcu("catoski","bornova sports",45,77,99);
-        System.out.println("adi: "+futbolcu.getFutbolcuAdi()+" penalti "+ futbolcu.getPenalti());
-        System.out.println("adi: "+basketbolcu.getBasketbolcuAdi()+" ucluk degeri: "+basketbolcu.getUcluk());
-        futbolcu.sporcuPuaniGoster();
-        basketbolcu.sporcuPuaniGoster();
+        Futbolcu futbolcu1 = new Futbolcu();
     }
 }
---
+++ src/com/company/Oyuncu.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Oyuncu {
+
+    private String oyuncuID;
+    private String oyuncuAdi;
+    private int skor;
+
+    public String getOyuncuID() {
+        return oyuncuID;
+    }
+
+    public void setOyuncuID(String oyuncuID) {
+        this.oyuncuID = oyuncuID;
+    }
+
+    public String getOyuncuAdi() {
+        return oyuncuAdi;
+    }
+
+    public void setOyuncuAdi(String oyuncuAdi) {
+        this.oyuncuAdi = oyuncuAdi;
+    }
+
+    public int getSkor() {
+        return skor;
+    }
+
+    public void setSkor(int skor) {
+        this.skor = skor;
+    }
+
+    public Oyuncu(String oyuncuID, String oyuncuAdi, int skor) {
+        this.oyuncuID = oyuncuID;
+        this.oyuncuAdi = oyuncuAdi;
+        this.skor = skor;
+    }
+}
